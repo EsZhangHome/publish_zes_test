afe16a5e33861ecc3299bafc612f077bf7782c0c
zhangensheng <zhangensheng@huice.com>
2023-10-12 08:55:43 +0800

    first commit

---
+++ ios/Classes/PublishZesTestPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface PublishZesTestPlugin : NSObject<FlutterPlugin>
+@end
---
+++ ios/Classes/PublishZesTestPlugin.m
@@ -0,0 +1,20 @@
+#import "PublishZesTestPlugin.h"
+
+@implementation PublishZesTestPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"publish_zes_test"
+            binaryMessenger:[registrar messenger]];
+  PublishZesTestPlugin* instance = [[PublishZesTestPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
